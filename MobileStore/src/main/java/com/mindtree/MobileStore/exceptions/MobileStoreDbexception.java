fa9a6aa1d36902550bd43eff9d360ce09faf2ac3
Ubuntu <DevOpsTeam5@DevOpsTeam5.kvdh40tkdpvu5caa5sytas315g.tx.internal.cloudapp.net>
2019-12-19 04:04:07 +0000

    modified c1

--- MobileStore/src/main/java/com/mindtree/MobileStore/exceptions/MobileStoreDbexception.java
+++ MobileStore/src/main/java/com/mindtree/MobileStore/exceptions/MobileStoreDbexception.java
@@ -4,22 +4,21 @@ public class MobileStoreDbexception extends AppException {
 
 	public MobileStoreDbexception() {
 		super();
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public MobileStoreDbexception(String arg0, Throwable arg1) {
 		super(arg0, arg1);
-		// TODO Auto-generated constructor stub
 	}
 
 	public MobileStoreDbexception(String arg0) {
 		super(arg0);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public MobileStoreDbexception(Throwable arg0) {
 		super(arg0);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 }
